94d9bc18dac87f782c54576e49b5ae9eb7b658f2
Nilay Shrimanwar <shrimanwar92@gmail.com>
2018-04-17 22:58:50 +0530

    added new tx logic

--- logic.js
+++ logic.js
@@ -1,156 +1,124 @@
+var factory = getFactory();
+var namespace = 'org.acme.loan';
 /**
  * Published transaction processor function.
  * @param {org.acme.loan.CreditLoan} tx The credited loan transaction instance.
  * @transaction
  */
 
- async function CreditLoan(tx) {   
- 	if(tx.borrowerRequest.isDone) {
- 		throw new Error("Your loan is already fulfilled."); 
- 	}
-    if(tx.lender.accountBalance < tx.amount) {
-      throw new Error("Insufficient Balance in your account"); 
+async function CreditLoan(tx) {
+    if (tx.borrowerRequest.isDone) {
+        throw new Error("Your loan is already fulfilled.");
+    }
+    if (tx.lender.accountBalance < tx.amount) {
+        throw new Error("Insufficient Balance in your account");
     }
     let remainingBorrowerLoan = tx.borrowerRequest.amountRequested - tx.borrowerRequest.amountFulfilled;
-    if(tx.amount > remainingBorrowerLoan) {
-      throw new Error(`Your amount exceeds the remaining loan amount of borrower: ${remainingBorrowerLoan}`);
+    if (tx.amount > remainingBorrowerLoan) {
+        throw new Error(`Your amount exceeds the remaining loan amount of borrower: ${remainingBorrowerLoan}`);
     }
     tx.lender.accountBalance -= tx.amount;
     tx.borrowerRequest.amountFulfilled += tx.amount;
-   
-   // check if lender is already present
-   let currentLender = tx.loan.lenders.filter(lndr => lndr.lender.userId == tx.lender.userId);
-   
-   
-   	if(currentLender.length > 0) {
-    	currentLender[0].amount += tx.amount;;
+
+    // check if lender is already present
+    let currentLender = tx.loan.lenders.filter(lndr => lndr.lender.userId == tx.lender.userId);
+
+    if (currentLender.length > 0) {
+        currentLender[0].amount += tx.amount;;
     } else {
-      	// create a concept resource to add to lenders array if lender is not present
-    	const details = getFactory().newConcept('org.acme.loan', 'LenderDetails');
-    	details.lender = tx.lender;
-    	details.amount = tx.amount;
-      	tx.loan.lenders.push(details);
+        // create a concept resource to add to lenders array if lender is not present
+        const details = getFactory().newConcept('org.acme.loan', 'LenderDetails');
+        details.lender = tx.lender;
+        details.amount = tx.amount;
+        tx.loan.lenders.push(details);
     }
-   
-    if(tx.borrowerRequest.amountFulfilled == tx.borrowerRequest.amountRequested) {
-      tx.borrowerRequest.isDone = true;
+
+    if (tx.borrowerRequest.amountFulfilled == tx.borrowerRequest.amountRequested) {
+        tx.borrowerRequest.isDone = true;
         tx.borrowerRequest.borrower.accountBalance = tx.loan.borrowerRequest.amountFulfilled;
         tx.borrowerRequest.borrower.total += 1;
     }
-    
-    let date = new Date();
-    tx.loan.endDate = new Date(date.setMonth(date.getMonth() + tx.loan.borrowerRequest.durationOfLoanInMonths));
-   
+
+    tx.loan.endDate = new Date(new Date().setMonth(new Date().getMonth() + tx.borrowerRequest.durationOfLoanInMonths));
+
     let lenderRegistry = await getParticipantRegistry('org.acme.loan.Lender');
-  await lenderRegistry.update(tx.lender);
-   
+    await lenderRegistry.update(tx.lender);
+
     let borrowerRegistry = await getParticipantRegistry('org.acme.loan.BorrowerRequest');
-  await borrowerRegistry.update(tx.borrowerRequest);
-   
+    await borrowerRegistry.update(tx.borrowerRequest);
+
     let br = await getParticipantRegistry('org.acme.loan.Borrower');
-  await br.update(tx.borrowerRequest.borrower);
-   
+    await br.update(tx.borrowerRequest.borrower);
+
     let assetRegistry = await getAssetRegistry('org.acme.loan.Loan');
     await assetRegistry.update(tx.loan);
- }
+}
 
 /**
- * Published transaction processor function.
- * @param {org.acme.loan.RepayLoan} tx The repaid loan transaction instance.
- * @transaction
- */
+* Published transaction processor function.
+* @param {org.acme.loan.RepayLoan} tx The repaid loan transaction instance.
+* @transaction
+*/
 async function RepayLoan(tx) {
-	if(tx.borrowerRequest.isRepaid) {
-      throw new Error("You have already repaid the loan."); 
+    if (tx.borrowerRequest.isRepaid) {
+        throw new Error("You have already repaid the loan.");
     }
-  if(tx.borrowerRequest.borrower.accountBalance < tx.amount) {
-      throw new Error("Insufficient Balance in your account"); 
+    if (tx.borrowerRequest.borrower.accountBalance < tx.amount) {
+        throw new Error("Insufficient Balance in your account");
     }
     tx.lender.accountBalance += tx.amount;
-    tx.borrowerRequest.borrower.accountBalance -= tx.amount; 
+    tx.borrowerRequest.borrower.accountBalance -= tx.amount;
     tx.borrowerRequest.amountRepaid += tx.amount;
-  
-  if(tx.borrowerRequest.amountFulfilled == tx.borrowerRequest.amountRepaid) {
-      tx.loan.borrowerRequest.isRepaid = true;
+
+    // check if lender is already present
+    let currentLender = tx.loan.lenders.filter(lndr => lndr.lender.userId == tx.lender.userId);
+    currentLender[0].repaid += tx.amount;
+
+    if (tx.borrowerRequest.amountFulfilled == tx.borrowerRequest.amountRepaid) {
+        tx.borrowerRequest.isRepaid = true;
     }
-  
+
     let lenderRegistry = await getParticipantRegistry('org.acme.loan.Lender');
-  await lenderRegistry.update(tx.lender);
-   
+    await lenderRegistry.update(tx.lender);
+
     let borrowerRegistry = await getParticipantRegistry('org.acme.loan.BorrowerRequest');
-  await borrowerRegistry.update(tx.borrowerRequest);
-   
+    await borrowerRegistry.update(tx.borrowerRequest);
+
     let br = await getParticipantRegistry('org.acme.loan.Borrower');
-  await br.update(tx.borrowerRequest.borrower);
-   
+    await br.update(tx.borrowerRequest.borrower);
+
     let assetRegistry = await getAssetRegistry('org.acme.loan.Loan');
     await assetRegistry.update(tx.loan);
-    
-}
-/**
- * Published transaction processor function.
- * @param {org.acme.loan.addNewLender} tx The credited loan transaction instance.
- * @transaction
- */
 
-async function addNewLender() {
-  
-    const factory = getFactory();
-    const namespace = 'org.acme.loan';
-  // get the borrower
-    const borrowerRegistry = await getParticipantRegistry(namespace + '.Borrower');
-    const borrower = await borrowerRegistry.get("b1");
-  
-  // get the lender
-    const lenderRegistry = await getParticipantRegistry(namespace + '.Lender');
-    const lender = await lenderRegistry.get("l2");
-  
-    const loanRegistry = await getAssetRegistry(namespace + '.Loan');
-    const loan = factory.newResource(namespace, 'Loan', 'loan2');
-    loan.borrower = borrower;
-    loan.lender = lender;
-    loan.startDate = new Date();
-    loan.endDate = new Date();
-    loan.status = 'CREDITED';
-    loan.amount = 0;
-    await loanRegistry.add(loan);
 }
 
 /**
- * Create the participants to use in the demo
- * @param {org.acme.loan.SetupDemo} setupDemo - the SetupDemo transaction
- * @transaction
- */
-async function setupDemo() { // eslint-disable-line no-unused-vars
-    console.log('setupDemo');
+* Published transaction processor function.
+* @param {org.acme.loan.RequestLoan} request The request loan transaction instance.
+* @transaction
+*/
 
-    const factory = getFactory();
-    const namespace = 'org.acme.loan';
-  
-    // create a borrower participant resource
-    const borrower = factory.newResource(namespace, 'Borrower', 'b1');
-    borrower.amountRequested = 1000;
-    borrower.amountFulfilled = 0;
+async function RequestLoan(request) {
+    // get the registry
+    const borrowerRequestRegistry = await getParticipantRegistry(namespace + '.BorrowerRequest');
+    const loanAssetRegistry = await getAssetRegistry(namespace + '.Loan');
 
-    // add the borrower
-    const borrowerRegistry = await getParticipantRegistry(namespace + '.Borrower');
-    await borrowerRegistry.add(borrower);
+    // create a borrower request resource
+    let requestId = `req-${Math.floor(Math.random()*90000) + 10000}`;
+    const borrowerRequest = factory.newResource(namespace, 'BorrowerRequest', requestId);
+    borrowerRequest.amountRequested = request.amountRequested;
+    borrowerRequest.loanRequirementPurpose = request.loanRequirementPurpose;
+    borrowerRequest.durationOfLoanInMonths = request.durationOfLoanInMonths;
+    borrowerRequest.borrower = request.borrower;
+    await borrowerRequestRegistry.add(borrowerRequest);
   
-    // create a lender participant resource
-    const lender = factory.newResource(namespace, 'Lender', 'l1');
-    lender.accountBalance = 10000;
-    
-    // add the lender participant resource
-    const lenderRegistry = await getParticipantRegistry(namespace +'.Lender');
-    await lenderRegistry.add(lender);
-  
-    const loanRegistry = await getAssetRegistry(namespace + '.Loan');
-    const loan = factory.newResource(namespace, 'Loan', 'loan1');
-    loan.borrower = borrower;
-    loan.lender = lender;
-    loan.startDate = new Date();
-    loan.endDate = new Date();
-    loan.status = 'CREDITED';
-    loan.amount = 0;
-    await loanRegistry.add(loan);
+      // create a loan resource
+    let currentDate = new Date();
+    let loanId = `loan-${Math.floor(Math.random()*90000) + 10000}`;
+    const loan = factory.newResource(namespace, 'Loan', loanId);
+    loan.borrowerRequest = borrowerRequest;
+    loan.lenders = [];
+    loan.startDate = currentDate;
+    loan.endDate = currentDate;
+    await loanAssetRegistry.add(loan);
 }
